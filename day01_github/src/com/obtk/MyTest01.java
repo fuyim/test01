2b8766be10494a8df132728c58d31634b031e8cc
fym <fym@fuyimin.com>
2021-02-22 16:13:57 +0800

    测试03

--- day01_github/src/com/obtk/MyTest01.java
+++ day01_github/src/com/obtk/MyTest01.java
@@ -3,8 +3,10 @@ package com.obtk;
 public class MyTest01 {
 
     public static void main(String[] args) {
-        System.out.println("第一条执行");
+        System.out.println("第一次执行");
         Student student = new Student("ls","223");
+        sendHello();
+        System.out.println("第二次执行");
     }
 
     static class Student{
@@ -15,7 +17,9 @@ public class MyTest01 {
             this.username = username;
             this.password = password;
         }
+    }
 
-
+    public static void sendHello(){
+        System.out.println("hello Github  ^。^!");
     }
 }
