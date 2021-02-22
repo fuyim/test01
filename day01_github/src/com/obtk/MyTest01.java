7943f30fe95ebed8a9d968a660fe99191c410544
fym <fym@fuyimin.com>
2021-02-22 16:09:27 +0800

    测试02

--- day01_github/src/com/obtk/MyTest01.java
+++ day01_github/src/com/obtk/MyTest01.java
@@ -4,11 +4,18 @@ public class MyTest01 {
 
     public static void main(String[] args) {
         System.out.println("第一条执行");
-        Student student = new Student();
+        Student student = new Student("ls","223");
     }
 
     static class Student{
         private String username;
         private String password;
+
+        public Student(String username, String password) {
+            this.username = username;
+            this.password = password;
+        }
+
+
     }
 }
